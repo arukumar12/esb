f37c9075fa97d563a55b4739e8c7c2e0ef3d5892
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-01-19 16:19:13 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int j, a;
+    printf("enter the number of times that u want to display hello word");
+    scanf("%d",a);
+    for (j=0; j<n; j++)
+    { printf("hello\n");
+    }
+   
+}
