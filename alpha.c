13e6c5a53c9acc882b30b164fc64a6d5c9c5a0be
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-01-19 16:14:06 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the letter");
+scanf("%d",&n);
+if(n>='a'&&n<='A')
+{
+printf("letter G is alphat");
+}
+else
+{
+printf("letter is not alphat");
+}
