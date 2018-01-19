bd754c6ce915b3f1bc82861ab140f0849876cfa9
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-01-19 16:36:01 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i=3,j=4,c=8;
+if(i>j)
+{
+printf("i is greatest number");
+}
+if(j>k)
+{
+printf("j is greatest number");
+}
+if(k>i)
+{
+printf("k is greatest number");
+}
+return(0);
