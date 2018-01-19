352fc5a7f1b1f7faf1864d83b7823a797bafc717
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-01-19 16:47:02 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int b[11],i,j,j,sum=0;
+scanf("%d%d",&i,&j);
+for(i=1;i<=i;i++)
+{
+  printf("%d\t",b[i]);
+}
+for(i=1;i<=j;i++)
+{
+  sum=sum+b[i];
+}
+  printf("%d",sum);
+}
