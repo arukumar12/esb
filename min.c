0d16b47c3982612e2ce03db9ed7274f749784a5b
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-01-25 16:20:29 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int b,a[20],i,m;
+printf("enter a number");
+scanf("%d",&n);
+printf("enter the numbers");
+for(i=0;i<n;i++)
+scanf("%d",&b[i]);
+m=b[0];
+for(i=1;i<n;i++)
+{
+if(m>b[i])
+m=a[i];
+}
+printf("%d",m);
+}
