154cf33020a0e39d5201f9c3aad96a58857a7543
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-01-25 16:50:25 +0530

    Create nxt.c

---
+++ nxt.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main()
+{
+int i,j,a[10],temp,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
